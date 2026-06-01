83a54ea6133c56fe2e8c1091c21f646c1498666b
SHIN YONG JUN <4rest528@naver.com>
2026-06-01 20:10:42 +0900

    Update main.cpp

--- Proto/tests/main.cpp
+++ Proto/tests/main.cpp
@@ -1,2 +1,18 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+int main(int argc, char** argv) {
+#ifdef _WIN32
+    SetConsoleOutputCP(65001);
+#endif
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+    int res = context.run();
+    if (context.shouldExit()) {
+        return res;
+    }
+    return res;
+}
